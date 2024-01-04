7d931823fcab6a8471dfd295f295d5d837e2f3ad
omer aydin <omeraydin2112@gmail.com>
2024-01-04 19:33:55 +0300

    project modified

--- video-rwitch/app/(browse)/_components/navbar/logo.tsx
+++ video-rwitch/app/(browse)/_components/navbar/logo.tsx
@@ -13,24 +13,27 @@ const font = Poppins({
 export const Logo = () => {
     return (
         <Link href={"/"}>
-            <div className="items-center hidden transition lg:flex gap-x-4 hover:opacity-75">
-                <div className="p-1 bg-white rounded-full">
-                <Image
-                    src={"/spooky.svg"}
-                    alt={"Logo"}
-                    height={"32"}
-                    width={"32"} />
+            <div className="flex items-center gap-x-4 hover:opacity-75">
+                <div className="p-1 mr-12 bg-white rounded-full shrink-0 lg:mr-0 lg:shrink">
+                    <Image
+                        src={"/spooky.svg"}
+                        alt={"Logo"}
+                        height={"32"}
+                        width={"32"} />
                 </div>
-                <div className={cn(font.className)}>
-                <p className="text-lg font-semibold">
-                    Rwitch
-                </p>
-                <p className="text-sm text-muted-foreground">
-                    Let&apos;s play
-                </p>
+                <div className={cn(
+                    "hidden lg:block",
+                    font.className
+                    )}>
+                    <p className="text-lg font-semibold">
+                        Rwitch
+                    </p>
+                    <p className="text-sm text-muted-foreground">
+                        Let&apos;s play
+                    </p>
+                </div>
             </div>
-            </div>
-            
+
         </Link>
 
     )
