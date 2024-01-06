d9d2f3ff626f8bac4134d575cef0b7f4e70b0724
omer aydin <omeraydin2112@gmail.com>
2024-01-06 07:14:19 +0300

    toggle

---
+++ video-rwitch/app/(browse)/_components/sidebar/toggle.tsx
@@ -0,0 +1,38 @@
+"use client"
+import { Button } from "@/components/ui/button";
+import { useSidebar } from "@/store/use-sidebar"
+import { ArrowLeftFromLine, ArrowRightFromLine } from "lucide-react";
+
+export const Toggle = () => {
+
+    const { collapsed, onExpand, onCollapse } = useSidebar((state) => state);
+
+    const label = collapsed ? "Expand" : "Collapse";
+    return (
+        <>
+            {collapsed && (
+                <div className="items-center justify-center hidden w-full pt-4 mb-4 lg:flex ">
+                    <Button 
+                        onClick={onExpand}
+                        className="h-auto p-2 "
+                        variant={"ghost"}>
+                        <ArrowRightFromLine className="w-4 h-4"/>
+                    </Button>
+                </div>
+            )}
+            {!collapsed && (
+                <div className="flex items-center w-full p-3 pl-6 mb-2">
+                    <p className="font-semibold text-primary">
+                        For you
+                    </p>
+                    <Button
+                        onClick={onCollapse}
+                        className="h-auto p-2 ml-auto "
+                        variant={"ghost"}>
+                        <ArrowLeftFromLine className="w-4 h-4" />
+                    </Button>
+                </div>
+            )}
+        </>
+    )
+}
