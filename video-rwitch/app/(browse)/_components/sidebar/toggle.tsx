f3e30c1bc4bb4603b79ddd1fffd5c7f3e467db23
omer aydin <omeraydin2112@gmail.com>
2024-01-06 08:11:57 +0300

    tooltip added hint

--- video-rwitch/app/(browse)/_components/sidebar/toggle.tsx
+++ video-rwitch/app/(browse)/_components/sidebar/toggle.tsx
@@ -2,6 +2,7 @@
 import { Button } from "@/components/ui/button";
 import { useSidebar } from "@/store/use-sidebar"
 import { ArrowLeftFromLine, ArrowRightFromLine } from "lucide-react";
+import { Hint } from "@/components/hint"
 
 export const Toggle = () => {
 
@@ -12,11 +13,11 @@ export const Toggle = () => {
         <>
             {collapsed && (
                 <div className="items-center justify-center hidden w-full pt-4 mb-4 lg:flex ">
-                    <Button 
+                    <Button
                         onClick={onExpand}
                         className="h-auto p-2 "
                         variant={"ghost"}>
-                        <ArrowRightFromLine className="w-4 h-4"/>
+                        <ArrowRightFromLine className="w-4 h-4" />
                     </Button>
                 </div>
             )}
@@ -25,12 +26,14 @@ export const Toggle = () => {
                     <p className="font-semibold text-primary">
                         For you
                     </p>
-                    <Button
-                        onClick={onCollapse}
-                        className="h-auto p-2 ml-auto "
-                        variant={"ghost"}>
-                        <ArrowLeftFromLine className="w-4 h-4" />
-                    </Button>
+                    <Hint label={label} side="right" asChild>
+                        <Button
+                            onClick={onCollapse}
+                            className="h-auto p-2 ml-auto "
+                            variant={"ghost"}>
+                            <ArrowLeftFromLine className="w-4 h-4" />
+                        </Button>
+                    </Hint>
                 </div>
             )}
         </>
