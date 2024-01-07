ee5c3770ac883a81eb11ce1df6d2bcab64b4db55
omer aydin <omeraydin2112@gmail.com>
2024-01-08 01:49:18 +0300

    add username page

---
+++ video-rwitch/app/(browse)/[username]/page.tsx
@@ -0,0 +1,18 @@
+interface UserPageProps {
+    params: {
+        username: string
+    }
+}
+
+const UserPage = ({
+    params
+}: UserPageProps) => {
+
+    return(
+        <div>
+            User: {params.username}
+        </div>
+    )
+}
+
+export default UserPage;
