a7fb79d2eb2d1cef7d9821852744de9b8944b7fc
omer aydin <omeraydin2112@gmail.com>
2024-01-07 07:35:24 +0300

    skeleton for toggle, wrapper

--- video-rwitch/app/(browse)/_components/sidebar/toggle.tsx
+++ video-rwitch/app/(browse)/_components/sidebar/toggle.tsx
@@ -3,6 +3,7 @@ import { Button } from "@/components/ui/button";
 import { useSidebar } from "@/store/use-sidebar"
 import { ArrowLeftFromLine, ArrowRightFromLine } from "lucide-react";
 import { Hint } from "@/components/hint"
+import { Skeleton } from "@/components/ui/skeleton";
 
 export const Toggle = () => {
 
@@ -39,5 +40,14 @@ export const Toggle = () => {
                 </div>
             )}
         </>
+    )
+}
+
+export const ToggleSkeleton = () => {
+    return (
+        <div className="items-center justify-between hidden w-full p-3 pl-6 mb-2 lg:flex">
+            <Skeleton className="h-6 w-[100px]"/>
+            <Skeleton className="w-6 h-6"/>
+        </div>
     )
 }
--- video-rwitch/app/(browse)/_components/sidebar/wrapper.tsx
+++ video-rwitch/app/(browse)/_components/sidebar/wrapper.tsx
@@ -2,6 +2,10 @@
 
 import { useSidebar } from "@/store/use-sidebar";
 import { cn } from "@/lib/utils";
+import { useState, useEffect } from "react";
+import { ToggleSkeleton } from "./toggle";
+import { RecommendedSkeleton } from "./recommended";
+import { useIsClient } from "usehooks-ts";
 
 interface WrapperProps {
     children: React.ReactNode;
@@ -10,11 +14,22 @@ interface WrapperProps {
 export const Wrapper = ({
     children
 }: WrapperProps) => {
-
+    const isClient = useIsClient();
     const { collapsed } = useSidebar((state) => state);
 
+
+    if (!isClient) {
+        return (
+            <aside className="fixed left-0 flex flex-col h-full w-[70px] lg:w-60 bg-background border-r border-[#2D2E35] z-50">
+                <ToggleSkeleton/>
+                <RecommendedSkeleton/>
+            </aside>
+        );
+    }
+
     return (
-        <aside className={cn("fixed left-0 flex flex-col h-full w-60 bg-background border-r border-[#2D2E35] z-50",
+        <aside className={cn(
+            "fixed left-0 flex flex-col h-full w-60 bg-background border-r border-[#2D2E35] z-50",
             collapsed && "w-[70px]")}>
             {children}
         </aside>
