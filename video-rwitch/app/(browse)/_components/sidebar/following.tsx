d628eb917935764700070ed2d3a6bab8b2c658ba
omer aydin <omeraydin2112@gmail.com>
2024-01-08 08:30:58 +0300

    following added

---
+++ video-rwitch/app/(browse)/_components/sidebar/following.tsx
@@ -0,0 +1,48 @@
+"use client"
+
+import { useSidebar } from "@/store/use-sidebar";
+import { Follow, User } from "@prisma/client";
+import { UserItem, UserItemSkeleton } from "./user-item";
+
+
+interface FollowingProps{
+    data: (Follow & {following: User})[]
+}
+
+export const Following = ({data}: FollowingProps)=>{
+
+    const {collapsed} = useSidebar((state) => state);
+
+    const showLabel = !collapsed && data.length > 0;
+
+    return(
+        <div>
+           {showLabel && (
+                <div className="pl-6 mb-4">
+                    <p className="text-sm text-muted-foreground">
+                        Following
+                    </p>
+                </div>
+            )}
+            <ul className="px-2 space-y-2 ">
+                {data.map((user) => (
+                    <UserItem 
+                    key={user.id}
+                    username={user.following.username}
+                    imageUrl={user.following.imageUrl}
+                    />
+                ))}
+            </ul>
+        </div>
+    )
+}
+
+export const FollowingSkeleton = () => {
+    return (
+        <ul className="px-2 pt-2 lg:pt-0">
+            {[...Array(3)].map((_, i) => (
+                <UserItemSkeleton key={i}/>
+            ))}
+        </ul>
+    )
+}
