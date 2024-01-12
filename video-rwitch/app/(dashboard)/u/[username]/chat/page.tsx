1008cbd57a706159351bba33e4114dc6ca9d6208
omer aydin <omeraydin2112@gmail.com>
2024-01-12 05:37:07 +0300

    chat setting page

---
+++ video-rwitch/app/(dashboard)/u/[username]/chat/page.tsx
@@ -0,0 +1,24 @@
+import { getSelf } from "@/lib/auth-service";
+import { getStreamByUserId } from "@/lib/stream-service";
+
+const ChatPage = async() => {
+
+    const self = await getSelf();
+    const stream = await getStreamByUserId(self.id);
+
+    if (!stream) {
+        throw new Error("Stream not found")
+    }
+
+    return (
+        <div className="p-6">
+            <div className="flex items-center justify-between mb-4 ">
+                <h1 className="text-2xl font-bold ">
+                    Chat Setting
+                </h1>
+            </div>
+        </div>
+    );
+};
+
+export default ChatPage;
