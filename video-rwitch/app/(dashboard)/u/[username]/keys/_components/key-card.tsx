e0f7eeafd0234c79ca2163d80e992ad2ca07a7aa
omer aydin <omeraydin2112@gmail.com>
2024-01-16 04:12:55 +0300

    key card v1

---
+++ video-rwitch/app/(dashboard)/u/[username]/keys/_components/key-card.tsx
@@ -0,0 +1,16 @@
+"use client"
+
+interface KeyCardProps {
+    value: string | null;
+};
+
+export const KeyCardProps = ({
+    value
+}: KeyCardProps) => {
+
+    return (
+        <div>
+            KeyCards
+        </div>
+    )
+}
