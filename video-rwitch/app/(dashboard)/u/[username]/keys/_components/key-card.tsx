ed725256e1372d8866789cfba2e8ac5796864086
omer aydin <omeraydin2112@gmail.com>
2024-01-16 04:33:42 +0300

    key card

--- video-rwitch/app/(dashboard)/u/[username]/keys/_components/key-card.tsx
+++ video-rwitch/app/(dashboard)/u/[username]/keys/_components/key-card.tsx
@@ -1,16 +1,46 @@
 "use client"
 
+import { useState } from "react";
+import { Input } from "@/components/ui/input";
+import { CopyButton } from "./copy-button";
+import { Button } from "@/components/ui/button";
+
 interface KeyCardProps {
     value: string | null;
 };
 
-export const KeyCardProps = ({
+export const KeyCard = ({
     value
 }: KeyCardProps) => {
 
+    const [show, setShow] = useState(false);
+
+
     return (
-        <div>
-            KeyCards
+        <div className="p-6 rounded-xl bg-muted">
+            <div className="flex items-start gap-x-10">
+                <p className="font-semibold shrink-0">
+                    Stream Key
+                </p>
+                <div className="w-full space-y-2 ">
+                    <div className="flex items-center w-full gap-x-2">
+                        <Input
+                            value={value || ""}
+                            type={show ? "text" : "password"}
+                            disabled
+                            placeholder="Stream Key"
+                        />
+                        <CopyButton value={value || ""} />
+                    </div>
+                    <Button
+                        onClick={() => setShow(!show)}
+                        size={"sm"}
+                        variant={"link"}
+                    >
+                        {show ? "Hide" : "Show"}
+                    </Button>
+                </div>
+            </div>
         </div>
     )
 }
