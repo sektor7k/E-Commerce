70353f12571b9349fe7ce9d90248572c4038e766
omer aydin <omeraydin2112@gmail.com>
2024-01-16 04:13:31 +0300

    add url keys v1 from keys page

--- video-rwitch/app/(dashboard)/u/[username]/keys/page.tsx
+++ video-rwitch/app/(dashboard)/u/[username]/keys/page.tsx
@@ -1,7 +1,33 @@
-const KeysPage = () => {
+import { Button } from "@/components/ui/button";
+import { UrlCard } from "./_components/url-card";
+import { getSelf } from "@/lib/auth-service";
+import { getStreamByUserId } from "@/lib/stream-service";
+import { KeyCardProps } from "./_components/key-card";
+
+
+const KeysPage = async () => {
+
+    const self = await getSelf();
+    const stream = await getStreamByUserId(self.id);
+
+    if (!stream) {
+        throw new Error("Stream not found");
+    }
+
     return (
-        <div>
-            Keys Page
+        <div className="p-6 ">
+            <div className="flex items-center justify-between mb-4">
+                <h1 className="text-2xl font-bold ">
+                    Keys & URLs
+                </h1>
+                <Button variant={"primary"}>
+                    Generate
+                </Button>
+            </div>
+            <div className="space-y-4 ">
+                <UrlCard  value={stream.serverUrl}/>
+                <KeyCardProps value={stream.streamKey}/>
+            </div>
         </div>
     )
 }
