3bcd6d007effd04d9297ca0a9e03c9f9077f123d
omer aydin <omeraydin2112@gmail.com>
2024-01-19 02:37:30 +0300

    chat header skeleton

--- video-rwitch/components/stream-player/chat-header.tsx
+++ video-rwitch/components/stream-player/chat-header.tsx
@@ -1,11 +1,25 @@
 "use client";
 
+import { Skeleton } from "../ui/skeleton";
+
 export const ChatHeader = () => {
     return (
         <div className="relative p-3 border-b ">
-            <p className="font-semibold text-center  text-primary">
+            {/* TODO: Togle Chat Sidebar */}
+            <p className="font-semibold text-center text-primary">
                 Stream Chat
             </p>
+            {/* TODO: Toggle Chat Community */}
+
+        </div>
+    )
+};
+
+export const ChatHeaderSkeleton = () => {
+    return (
+        <div className="relative hidden p-3 border-b  md:block">
+            <Skeleton className="absolute w-6 h-6  left-3 top-3"/>
+            <Skeleton className="h-6 mx-auto  w-28"/>
         </div>
     )
 }
