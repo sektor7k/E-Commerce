83bef595cb1b0674957cbd70c9c85f0a7f097e25
omer aydin <omeraydin2112@gmail.com>
2024-01-18 08:49:20 +0300

    stream player video

---
+++ video-rwitch/components/stream-player/video.tsx
@@ -0,0 +1,44 @@
+"use client"
+
+import { ConnectionState, Track } from "livekit-client";
+import {
+    useConnectionState,
+    useRemoteParticipant,
+    useTracks,
+} from "@livekit/components-react"
+import { OfflineVideo } from "./offline-video";
+import { LoadingVideo } from "./loading-video";
+
+interface VideoProps {
+    hostName: string;
+    hostIdentity: string;
+};
+
+export const Video = ({
+    hostName,
+    hostIdentity,
+}: VideoProps) => {
+
+    const connectionState = useConnectionState();
+    const participant = useRemoteParticipant(hostIdentity);
+    const tracks = useTracks([
+        Track.Source.Camera,
+        Track.Source.Microphone,
+    ]).filter((track)=> track.participant.identity === hostIdentity);
+
+    let content;
+
+    if ( !participant && connectionState === ConnectionState.Connected) {
+        content = <OfflineVideo username={hostName}/>;
+    } else if (!participant || tracks.length === 0) {
+        content = <LoadingVideo label={connectionState}/>
+    } else {
+        content = <p>Live video</p>
+    }
+
+    return (
+        <div className="relative border-b aspect-video group">
+            {content}
+        </div>
+    )
+}
