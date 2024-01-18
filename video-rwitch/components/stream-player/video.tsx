912bf29ef683702ba97e73941daa5d2567dd27b9
omer aydin <omeraydin2112@gmail.com>
2024-01-18 10:01:57 +0300

    fullscreen constrol component

--- video-rwitch/components/stream-player/video.tsx
+++ video-rwitch/components/stream-player/video.tsx
@@ -8,6 +8,7 @@ import {
 } from "@livekit/components-react"
 import { OfflineVideo } from "./offline-video";
 import { LoadingVideo } from "./loading-video";
+import { LiveVideo } from "./live-video";
 
 interface VideoProps {
     hostName: string;
@@ -33,7 +34,7 @@ export const Video = ({
     } else if (!participant || tracks.length === 0) {
         content = <LoadingVideo label={connectionState}/>
     } else {
-        content = <p>Live video</p>
+        content = <LiveVideo participant={participant}/>
     }
 
     return (
