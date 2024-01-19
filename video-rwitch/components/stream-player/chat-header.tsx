3bcd269ead81aa64e275098ff5e96d6761ebfa2c
omer aydin <omeraydin2112@gmail.com>
2024-01-19 05:54:48 +0300

    toggle add chat

--- video-rwitch/components/stream-player/chat-header.tsx
+++ video-rwitch/components/stream-player/chat-header.tsx
@@ -1,15 +1,21 @@
 "use client";
 
 import { Skeleton } from "../ui/skeleton";
+import { ChatToggle } from "./chat-toggle";
+import { VariantToggle } from "./variant-toggle";
 
 export const ChatHeader = () => {
     return (
         <div className="relative p-3 border-b ">
-            {/* TODO: Togle Chat Sidebar */}
+            <div className="absolute hidden left-2 top-2 lg:block">
+            <ChatToggle/>
+            </div>
             <p className="font-semibold text-center text-primary">
                 Stream Chat
             </p>
-            {/* TODO: Toggle Chat Community */}
+           <div className="absolute  right-2 top-2">
+                <VariantToggle />
+           </div>
 
         </div>
     )
@@ -17,9 +23,9 @@ export const ChatHeader = () => {
 
 export const ChatHeaderSkeleton = () => {
     return (
-        <div className="relative hidden p-3 border-b  md:block">
-            <Skeleton className="absolute w-6 h-6  left-3 top-3"/>
-            <Skeleton className="h-6 mx-auto  w-28"/>
+        <div className="relative hidden p-3 border-b md:block">
+            <Skeleton className="absolute w-6 h-6 left-3 top-3"/>
+            <Skeleton className="h-6 mx-auto w-28"/>
         </div>
     )
 }
--- video-rwitch/components/stream-player/index.tsx
+++ video-rwitch/components/stream-player/index.tsx
@@ -7,6 +7,7 @@ import { Video } from "./video";
 import { useChatSidebar } from "@/store/use-chat-sidebar";
 import { cn } from "@/lib/utils";
 import { Chat } from "./chat";
+import { ChatToggle } from "./chat-toggle";
 interface StreamPlayersProps {
     user: User & { stream: Stream | null };
     stream: Stream;
@@ -36,6 +37,11 @@ export const StreamPlayer = ({
 
     return (
         <>
+            {collapsed && (
+                <div className="hidden lg:block fixed top-[100px] right-2 z-50">
+                    <ChatToggle />
+                </div>
+            )}
             <LiveKitRoom
                 serverUrl={process.env.NEXT_PUBLIC_LIVEKIT_WS_URL}
                 token={token}
@@ -54,7 +60,7 @@ export const StreamPlayer = ({
                     "col-span-1",
                     collapsed && "hidden"
                 )}>
-                    <Chat 
+                    <Chat
                         viewerName={name}
                         hostName={user.username}
                         hostIdentity={user.id}
