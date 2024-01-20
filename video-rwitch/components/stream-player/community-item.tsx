f04fadbe2740299670291eb657f1ac62edacb5ba
omer aydin <omeraydin2112@gmail.com>
2024-01-20 09:35:44 +0300

    add block user community

--- video-rwitch/components/stream-player/community-item.tsx
+++ video-rwitch/components/stream-player/community-item.tsx
@@ -1,8 +1,13 @@
 "use client"
 
+import { toast } from "sonner";
+import { useTransition } from "react";
+import { MinusCircle } from "lucide-react";
 
+import { Hint } from "../hint";
+import { onBlock } from "@/actions/block";
 import { cn, stringToColor } from "@/lib/utils";
-
+import { Button } from "../ui/button";
 
 interface CommunityItemProps {
     hostName: string;
@@ -18,23 +23,41 @@ export const CommunityItem = ({
     participantIdentity
 }: CommunityItemProps) => {
 
-   
+    const [isPending, startTransition] = useTransition();
 
     const color = stringToColor(participantName || "");
     const isSelf = participantName === viewerName;
     const isHost = viewerName === hostName;
 
-   
+    const handleBlock = () => {
+        if (!participantName || isSelf || !isHost) return;
+
+        startTransition(() => {
+            onBlock(participantIdentity)
+                .then(() => toast.success(`Blocked ${participantName}`))
+                .catch(() => toast.error("Something went wrong"));
+        });
+    }
 
     return (
         <div className={cn(
             "group flex items-center justify-between w-full p-2 rounded-md text-sm hover:bg-white/5",
-           
+            isPending && "opacity-50 pointer-events-none"
         )}>
             <p style={{ color: color }}>
                 {participantName}
             </p>
-            
+            {isHost && !isSelf && (
+                <Hint label="Block">
+                    <Button
+                        variant={"ghost"}
+                        disabled={isPending}
+                        onClick={handleBlock}
+                        className="w-auto h-auto p-1 transition opacity-0 group-hover:opacity-100">
+                        <MinusCircle className="w-4 h-4 text-muted-foreground"/>
+                    </Button>
+                </Hint>
+            )}
         </div>
     )
 }
