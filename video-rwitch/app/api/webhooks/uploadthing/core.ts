d0b5074b13df5f34c48bbc0273cb05b50035f5f5
omer aydin <omeraydin2112@gmail.com>
2024-01-22 10:45:13 +0300

    upload core upgrade

--- video-rwitch/app/api/webhooks/uploadthing/core.ts
+++ video-rwitch/app/api/webhooks/uploadthing/core.ts
@@ -1,25 +1,35 @@
+import { metadata } from "@/app/layout";
+import { getSelf } from "@/lib/auth-service";
+import { db } from "@/lib/db";
 import { createUploadthing, type FileRouter } from "uploadthing/next";
- 
+
 const f = createUploadthing();
- 
-const auth = (req: Request) => ({ id: "fakeId" }); 
- 
+
+
 export const ourFileRouter = {
-  imageUploader: f({ image: { maxFileSize: "4MB" } })
-    .middleware(async ({ req }) => {
-      const user = await auth(req);
- 
-      if (!user) throw new Error("Unauthorized");
- 
-      return { userId: user.id };
+    thumbnailUploader: f({
+        image: {
+            maxFileSize: "4MB",
+            maxFileCount: 1
+        }
     })
-    .onUploadComplete(async ({ metadata, file }) => {
-      console.log("Upload complete for userId:", metadata.userId);
- 
-      console.log("file url", file.url);
- 
-      return { uploadedBy: metadata.userId };
-    }),
+        .middleware(async () => {
+            const self = await getSelf();
+            return { user: self }
+        })
+        .onUploadComplete(async ({ metadata, file }) => {
+            await db.stream.update({
+                where: {
+                    userId: metadata.user.id,
+                },
+                data: {
+                    thumbnailUrl: file.url,
+                },
+            });
+
+            return { fileUrl: file.url }
+        })
+
 } satisfies FileRouter;
- 
+
 export type OurFileRouter = typeof ourFileRouter;
