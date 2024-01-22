150e19721e432393a3535c21fc1f0519209cfc8b
omer aydin <omeraydin2112@gmail.com>
2024-01-22 11:54:30 +0300

    thumbnill add info-modal

--- video-rwitch/components/stream-player/info-card.tsx
+++ video-rwitch/components/stream-player/info-card.tsx
@@ -65,6 +65,7 @@ export const InfoCard = ({
                                         fill
                                         src={thumbnailUrl}
                                         alt={name}
+                                        className="onject-cover"
                                     />
                                 </div>
                             )}
--- video-rwitch/components/stream-player/info-modal.tsx
+++ video-rwitch/components/stream-player/info-modal.tsx
@@ -9,11 +9,16 @@ import {
     DialogTitle,
     DialogTrigger
 } from "@/components/ui/dialog"
+import { UploadDropzone } from "@/lib/uploadthing";
 import { Button } from "@/components/ui/button";
 import { Label } from "@/components/ui/label";
 import { Input } from "@/components/ui/input";
 import { updateStream } from "@/actions/stream";
 import { toast } from "sonner";
+import { useRouter } from "next/navigation";
+import { Hint } from "../hint";
+import { Trash } from "lucide-react";
+import Image from "next/image";
 
 interface InfoModalProps {
     initialName: string;
@@ -25,9 +30,12 @@ export const InfoModal = ({
     initialThumbnailUrl
 }: InfoModalProps) => {
 
+    const router = useRouter();
     const closeRef = useRef<ElementRef<"button">>(null);
     const [name, setName] = useState(initialName);
     const [isPending, startTransition] = useTransition();
+
+    const [thumbnailUrl, setThumbnailUrl] = useState(initialThumbnailUrl);
 
     const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
@@ -72,6 +80,52 @@ export const InfoModal = ({
                             onChange={onChange}
                             value={name}
                         />
+                    </div>
+                    <div className="space-y-2 ">
+                        <Label>
+                            Thumbnail
+                        </Label>
+                        {thumbnailUrl ? (
+                            <div className="relative overflow-hidden border aspect-video rounded-xl border-white/10">
+                                <div className=" absolute top-2 right-2 z-[10]">
+                                    <Hint label="Remove thumbnail" side="left" asChild>
+                                    <Button
+                                        type="button"
+                                        disabled={isPending}
+                                        onClick={() => {}}
+                                        className=" h-auto w-auto p-1.5"
+                                    >
+                                        <Trash  className="w-4 h-4 "/>
+                                    </Button>
+                                    </Hint>
+                                </div>
+                                <Image 
+                                    alt="Thumbnail"
+                                    src={thumbnailUrl}
+                                    fill
+                                    className="object-cover "
+                                />
+                            </div>
+
+                        ) : (
+                            <div className="border rounded-xl outline-dashed outline-muted">
+                                <UploadDropzone
+                                    endpoint="thumbnailUploader"
+                                    appearance={{
+                                        label: {
+                                            color: "#FFFFFF"
+                                        },
+                                        allowedContent: {
+                                            color: "#FFFFFF"
+                                        }
+                                    }}
+                                    onClientUploadComplete={(res) => {
+                                        setThumbnailUrl(res?.[0]?.url);
+                                        router.refresh();
+                                    }}
+                                />
+                            </div>
+                        )}
                     </div>
                     <div className="flex justify-between ">
                         <DialogClose ref={closeRef} asChild>
