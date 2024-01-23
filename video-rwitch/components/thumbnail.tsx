95b98ca267d6ca5e548002188d391302a6795fa2
omer aydin <omeraydin2112@gmail.com>
2024-01-24 02:58:04 +0300

    thumbnail

---
+++ video-rwitch/components/thumbnail.tsx
@@ -0,0 +1,48 @@
+import { UserAvatar } from "./user-avatar";
+import Image from "next/image";
+
+interface ThumbnailProps {
+    src: string | null;
+    fallback: string;
+    isLive: boolean;
+    username: string;
+};
+
+export const Thumbnail = ({
+    src,
+    fallback,
+    isLive,
+    username
+}: ThumbnailProps) => {
+    let content;
+
+    if (!src) {
+        content = (
+            <div className="flex flex-col items-center justify-center w-full h-full transition-transform rounded-md bg-background gap-y-4 group-hover:translate-x-2 group-hover:-translate-y-1">
+                <UserAvatar
+                    size="lg"
+                    showBadge
+                    username={username}
+                    imageUrl={fallback}
+                    isLive={isLive}
+                />
+            </div>
+        )
+    } else {
+        content = (
+            <Image
+                src={src}
+                fill
+                alt="Thumbnail"
+                className="object-cover transition-transform rounded-md group-hover:translate-x-2 group-hover:-translate-y-2"
+            />
+        )
+    }
+
+    return (
+        <div className="relative rounded-md cursor-pointer group aspect-video">
+            <div className="absolute inset-0 flex items-center transition-opacity bg-blue-600 rounded-md opacity-0 group-hover:opacity-100" />
+            {content}
+        </div>
+    )
+}
