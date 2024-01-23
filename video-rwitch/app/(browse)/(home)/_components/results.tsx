a06358fdc30979531e4525223fe3bcc4990411fe
omer aydin <omeraydin2112@gmail.com>
2024-01-24 02:57:31 +0300

    result modified

--- video-rwitch/app/(browse)/(home)/_components/results.tsx
+++ video-rwitch/app/(browse)/(home)/_components/results.tsx
@@ -1,7 +1,30 @@
-export const Results = () => {
+import { getStreams } from "@/lib/feed-service"
+import { ResultCard } from "./result-card";
+
+export const Results = async () => {
+    const data = await getStreams();
+
+
     return (
         <div>
-            Results
+            <h2 className="mb-4 text-lg font-semibold ">
+                Streams we think you&apos;ll like
+            </h2>
+            {data.length === 0 && (
+                <div className="text-sm text-muted-foreground">
+                    No streams found
+                </div>
+            )}
+            <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5">
+                {data.map((result) => (
+                    
+                        <ResultCard
+                            key={result.id}
+                            data={result}
+                        />
+                    
+                ))}
+            </div>
         </div>
     )
 }
@@ -9,7 +32,7 @@ export const Results = () => {
 export const ResultsSkeleton = () => {
     return (
         <div>
-            
+
         </div>
     )
 }
