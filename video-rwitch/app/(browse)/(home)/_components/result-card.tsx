0fb0f5972c3408cba68750122dc0e8e50aade613
omer aydin <omeraydin2112@gmail.com>
2024-01-24 03:51:21 +0300

    user and card skeleton

--- video-rwitch/app/(browse)/(home)/_components/result-card.tsx
+++ video-rwitch/app/(browse)/(home)/_components/result-card.tsx
@@ -1,9 +1,17 @@
-import { Thumbnail } from "@/components/thumbnail";
+import { LiveBadge } from "@/components/live-badge";
+import { Thumbnail, ThumbnailSkeleton } from "@/components/thumbnail";
+import { UserAvatar, UserAvatarSkeleton } from "@/components/user-avatar";
 import { User, Stream } from "@prisma/client";
 import Link from "next/link";
+import { Skeleton } from "@/components/ui/skeleton";
 
 interface ResultCardProps {
-    data: Stream & { user: User };
+    data: {
+        user: User;
+        isLive: boolean;
+        name: string;
+        thumbnailUrl: string | null;
+    }
 };
 
 export const ResultCard = ({
@@ -13,13 +21,49 @@ export const ResultCard = ({
     return (
         <Link href={`/${data.user.username}`}>
             <div className="w-full h-full space-y-4 ">
-                <Thumbnail 
+                <Thumbnail
                     src={data.thumbnailUrl}
                     fallback={data.user.imageUrl}
                     isLive={data.isLive}
                     username={data.user.username}
                 />
+                {data.isLive && (
+                    <div className="absolute transition-transform top-2 left-2 group-hover:translate-x-2 group-hover:-translate-y-2">
+                        <LiveBadge />
+                    </div>
+                )}
+                <div className="flex gap-x-3">
+                    <UserAvatar
+                        username={data.user.username}
+                        imageUrl={data.user.imageUrl}
+                        isLive={data.isLive}
+                    />
+                    <div className="flex flex-col overflow-hidden text-sm ">
+                        <p className="font-semibold truncate hover:text-blue-500">
+                            {data.name}
+                        </p>
+                        <p className=" text-muted-foreground">
+                            {data.user.username}
+                        </p>
+                    </div>
+                </div>
             </div>
         </Link>
     )
-}
+};
+
+export const ResultCardSkeleton = () => {
+
+    return (
+        <div className="w-full h-full space-y-4 ">
+            <ThumbnailSkeleton />
+            <div className="flex gap-x-3">
+                <UserAvatarSkeleton />
+                <div className="flex flex-col gap-y-1">
+                    <Skeleton className="w-32 h-4" />
+                    <Skeleton className="w-24 h-3" />
+                </div>
+            </div>
+        </div>
+    );
+};
