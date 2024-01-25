57e3331a4872d56d1bd0f17056bd83557359d931
omer aydin <omeraydin2112@gmail.com>
2024-01-25 06:29:44 +0300

    result card search

---
+++ video-rwitch/app/(browse)/search/_components/result-card.tsx
@@ -0,0 +1,45 @@
+import { Thumbnail } from "@/components/thumbnail";
+import { VerifiedMark } from "@/components/verified-mark";
+import { Stream, User } from "@prisma/client";
+import Link from "next/link";
+import { formatDistanceToNow } from "date-fns";
+
+interface ResultCardProps {
+    data: Stream & { user: User };
+};
+
+export const ResultCard = ({
+    data
+}: ResultCardProps) => {
+
+    return (
+        <Link href={`/${data.user.username}`}>
+            <div className="flex w-full gap-x-4">
+                <div className=" relative h-[9rem] w-[16rem]">
+                    <Thumbnail
+                        src={data.thumbnailUrl}
+                        fallback={data.user.imageUrl}
+                        isLive={data.isLive}
+                        username={data.user.username}
+                    />
+                </div>
+                <div className="space-y-1 ">
+                    <div className="flex items-center gap-x-2">
+                        <p className="text-lg font-bold cursor-pointer hover:text-blue-500">
+                            {data.user.username}
+                        </p>
+                        <VerifiedMark />
+                    </div>
+                    <p className="text-sm  text-muted-foreground">
+                        {data.name}
+                    </p>
+                    <p className="text-sm  text-muted-foreground">
+                        {formatDistanceToNow(new Date(data.updatedAt), {
+                            addSuffix: true
+                        })}
+                    </p>
+                </div>
+            </div>
+        </Link>
+    )
+}
