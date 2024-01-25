ccd760fec6c599f33c9ff3ef69919bb22ccfd9d2
omer aydin <omeraydin2112@gmail.com>
2024-01-25 07:53:42 +0300

    add data comminity page

--- video-rwitch/app/(dashboard)/u/[username]/community/page.tsx
+++ video-rwitch/app/(dashboard)/u/[username]/community/page.tsx
@@ -1,9 +1,30 @@
+import { Payment, columns } from "./_components/columns";
+import { DataTable } from "./_components/data-table";
+
+
+async function getData(): Promise<Payment[]> {
+    return [
+      {
+        id: "728ed52f",
+        amount: 100,
+        status: "pending",
+        email: "m@example.com",
+      },
+
+    ]
+  }
 
 const Community = async () => {
+    const data = await getData();
 
     return (
-        <div>
-            Community Page
+        <div className="p-6 ">
+            <div className="mb-4 ">
+                <h1 className="text-2xl font-bold ">
+                    Community Settings
+                </h1>
+            </div>
+            <DataTable columns={columns} data={data} />
         </div>
     )
 }
