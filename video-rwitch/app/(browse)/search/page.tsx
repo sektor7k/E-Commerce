f4c1c1ee102949168eb789b0c6c4b6a265e92561
omer aydin <omeraydin2112@gmail.com>
2024-01-25 06:28:56 +0300

    search page

--- video-rwitch/app/(browse)/search/page.tsx
+++ video-rwitch/app/(browse)/search/page.tsx
@@ -1,9 +1,27 @@
-const SearchPage = () => {
+import { redirect } from "next/navigation";
+import { Results, ResultsSkeleton } from "./_components/results";
+import { Suspense } from "react";
+
+interface SearchPageProps {
+    searchParams: {
+        term?: string;
+    };
+};
+
+const SearchPage = ({
+    searchParams,
+}: SearchPageProps) => {
+    if (!searchParams.term) {
+        redirect("/");
+    }
     return (
-        <div>
-            Search Page
+        <div className="h-full p-8 mx-auto max-w-screen-2xl">
+            <Suspense fallback={<ResultsSkeleton />}>
+                <Results
+                    term={searchParams.term} />
+            </Suspense>
         </div>
-    )
-}
+    );
+};
 
 export default SearchPage;
