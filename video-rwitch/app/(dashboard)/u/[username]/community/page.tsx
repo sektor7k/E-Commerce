fea11878193df65c4350a5ef5fbc35bd5ba799fe
omer aydin <omeraydin2112@gmail.com>
2024-01-26 10:04:38 +0300

    2:commminity page

--- video-rwitch/app/(dashboard)/u/[username]/community/page.tsx
+++ video-rwitch/app/(dashboard)/u/[username]/community/page.tsx
@@ -1,32 +1,32 @@
-import { Payment, columns } from "./_components/columns";
+import { getBlockedUsers } from "@/lib/block-service";
+import { columns } from "./_components/columns";
 import { DataTable } from "./_components/data-table";
+import { format } from "date-fns";
 
 
-async function getData(): Promise<Payment[]> {
-    return [
-      {
-        id: "728ed52f",
-        amount: 100,
-        status: "pending",
-        email: "m@example.com",
-      },
 
-    ]
-  }
 
 const Community = async () => {
-    const data = await getData();
+  const blockedUsers = await getBlockedUsers();
 
-    return (
-        <div className="p-6 ">
-            <div className="mb-4 ">
-                <h1 className="text-2xl font-bold ">
-                    Community Settings
-                </h1>
-            </div>
-            <DataTable columns={columns} data={data} />
-        </div>
-    )
+  const formattedData = blockedUsers.map((block) => ({
+    ...block,
+    userId: block.blocked.id,
+    imageUrl: block.blocked.imageUrl,
+    username: block.blocked.username,
+    createdAt: format(new Date(block.blocked.createdAt), "dd/MM/yyyy")
+  }))
+
+  return (
+    <div className="p-6 ">
+      <div className="mb-4 ">
+        <h1 className="text-2xl font-bold ">
+          Community Settings
+        </h1>
+      </div>
+      <DataTable columns={columns} data={formattedData} />
+    </div>
+  )
 }
 
 export default Community;
